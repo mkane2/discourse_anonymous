760293082ded479ff0963e04444c7ab627a9f895
mkane2 <mkane2@albany.edu>
2022-07-03 15:28:33 -0400

    update user-info

---
+++ javascripts/discourse/app/components/user-info.js
@@ -0,0 +1,27 @@
+import Component from "@ember/component";
+import { alias } from "@ember/object/computed";
+import discourseComputed from "discourse-common/utils/decorators";
+import { userPath } from "discourse/lib/url";
+import { prioritizeNameInUx } from "discourse/lib/settings";
+
+export default Component.extend({
+  classNameBindings: [":user-info", "size"],
+  attributeBindings: ["data-username"],
+  size: "small",
+  // "data-username": alias("user.username"),
+  "data-username": "nonnie",
+  // includeLink: true,
+  // includeAvatar: true,
+  includeLink: false,
+  includeAvatar: false,
+
+  @discourseComputed("user.username")
+  userPath(username) {
+    return userPath(username);
+  },
+
+  @discourseComputed("user.name")
+  nameFirst(name) {
+    return prioritizeNameInUx(name);
+  },
+});
